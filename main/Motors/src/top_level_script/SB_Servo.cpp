259b3b572207e4a8b939ec0135fb11ac3302a882
ahellerjones <hellerar@vt.edu>
2020-11-21 01:54:51 -0500

    Began expanding the functionality for SB_Servo.cpp & .hpp by adding member methods that wrap the maestro a bit

--- main/Motors/src/top_level_script/SB_Servo.cpp
+++ main/Motors/src/top_level_script/SB_Servo.cpp
@@ -6,7 +6,83 @@
 
 #include "SB_Servo.hpp"
 
+// Here the maestro is initialized to Serial1 on the Teensy, this is just one of 8 ports 
+// pretty neat. I suppose this is how static private variables are initialized
 MiniMaestro SB_Servo::maestro(Serial1);
+
 SB_Servo::SB_Servo() { 
-	// pass 
+	Serial1.begin(9600);
 }	
+
+
+SB_Servo::SB_Servo(int channel) { 
+	channelNum = channel;	
+	Serial1.begin(9600);
+}
+
+SB_Servo::SB_Servo(int minDegree, int maxDegree, int channel) { 
+	this->maxDegree = maxDegree;
+	this->minDegree = minDegree;
+	channelNum = channel;	
+	Serial1.begin(9600);
+}
+
+bool SB_Servo::setChannel(int channel) { 
+	if (channelNum >= 0 && channelNum <= 127) { 
+		channelNum = channel;	
+		return true;
+	} else {
+		return false;
+	}
+}
+
+bool SB_Servo::setMaximumAngle(int maximum) { 
+	if (maximum >= 0 && maximum <= 180 && maximum > minDegree) { 
+		maxDegree = maximum;
+		return true;
+	} else { 
+		return false;
+	}
+}
+
+bool SB_Servo::setMinimumAngle(int minimum) { 
+	if (minimum >= 0 && minimum <= 180 && minimum < maxDegree) { 
+		minDegree = minimum;
+		return true;
+	} else { 
+		return false;
+	}
+}
+
+
+bool SB_Servo::rotateToDegrees(float degree) { 
+	if (degree > maxDegree) { 
+	   degree = maxDegree;	
+	} else if (degree < minDegree) { 
+		degree = minDegree;
+	}
+
+	int msToWrite; // Value to write to the maestro
+	if (channelNum == -1) { 
+		//throw std::runtime_error("Attempted to move Servo without assigning channel number");
+		// figure out something to do, arduino compiler doesn't like to throw errors 
+		// I would normally make a std Serial output, but I don't want the code to assume
+		// what Serial ports are going to be open. Perhaps we can return a boolean indicating 
+		// whether or not the rotate was successful
+		return false;
+	} else { 
+		msToWrite = (int) map(degree, minDegree, maxDegree, 
+				MAESTRO_SERVOANGLE_MS_MIN, MAESTRO_SERVOANGLE_MS_MAX);
+		maestro.setTarget(channelNum, msToWrite);
+		return true;
+	}
+}
+
+
+
+
+
+
+
+
+
--- main/Motors/src/top_level_script/SB_Servo.hpp
+++ main/Motors/src/top_level_script/SB_Servo.hpp
@@ -15,11 +15,94 @@
  */
 
 #include <PololuMaestro.h>
+// #include <stdexcept> arduino compiler doesnt like to throw errors
+
+#define MAESTRO_SERVOANGLE_MS_MAX 8000 // The max servo angle in milliseconds
+#define MAESTRO_SERVOANGLE_MS_MIN 4000 // The min servo angle in milliseconds
 
 class SB_Servo { 
 	private: 
+		// We make the maestro static so that it's shared across all instances 
+		// of Servos
 		static MiniMaestro maestro;
+		int channelNum = -1; // default value for the channel, 
+						     // indicates the channel has not been assigned
+		int maxDegree = 180; // the maximum degree the servo can rotate to 
+		int minDegree = 0; // the minimum degree the servo can rotate to  
 
-   public: 
-    SB_Servo();
+   	public: 
+		/** 
+		 * Default constructor for the servo, I don't think 
+		 * this guy really needs to do anything
+		 * 
+		 * Begins the Serial monitor on Serial1. This could be changed in the future
+		 * to be able to dictated by .xml or .yml file
+		 */
+   		SB_Servo();
+
+		/**
+		 * Sets the channelNumber of a particular servo 
+		 * @param channel the channel number to use, which is the pin on
+		 * the maestro that the servo is connected to 
+		 * Begins the Serial monitor on Serial1. This could be changed in the future
+		 * to be able to dictated by .xml or .yml file
+		 */
+		SB_Servo(int);
+
+		/**
+		 * Sets the channelNumber of a particular servo 
+		 * Also sets the minimum and maximum degrees 
+		 * @param channel -- the channel number to use, which is the pin on the maestro to use
+		 * @param minDegr -- the minimum degree the servo can rotate to 
+		 * @param maxDegr -- the maximum degree the servo can rotate to 
+		 *
+		 * Begins the Serial monitor on Serial1. This could be changed in the future
+		 * to be able to dictated by .xml or .yml file
+		 */
+		SB_Servo(int minDegr, int maxDegr, int channel);
+
+
+		/** 
+		 * Sets the channel number for a particular system. Pretty straight forward
+		 *
+		 * Requires that the channel setting is within the  bounds for the maestro, 
+		 * 0 and 127.  
+		 * TODO: change 127 to the number of channels available in the MicroMaestro, 
+		 * obviously we don't have 127 channels, but there is a max num
+		 * @param channel -- the channel to assign this servo to 
+		 * @return whether or not the channel was set properly, the method only returns flase
+		 * if your provide a bad channel number
+		 */
+		bool setChannel(int);
+
+		/** 
+		 * Sets the max angle
+		 * @param maximum -- the value to set to maxDegrees
+		 * 						which dictates the max value the servo can rotate to 
+		 * For now, you can't set a max angle over 180 or under 0 
+		 * It also cannot be set less than or equal to the minimum
+		 * @return whether or not the passed value was within the acceptable range
+		 */
+		bool setMaximumAngle(int);
+
+		/** 
+		 * Sets the min angle
+		 * @param minimum --  the value to set to minDegrees
+		 * 						which dictates the min value the servo can rotate to 
+		 * For now, you can't set a max angle over 180 or under 0 
+		 * It also cannot be set greater or equal to than the maximum 
+		 * @return whether or not the passed value was within the acceptable range
+		 */
+		bool setMinimumAngle(int);
+		
+		/** Rotates this servo to a specific degrees, 0-180, or within the minimum
+		 * and maximum values as dictated by the member values
+		 *
+		 * @param degrees -- the degrees to rotate to 
+		 * @return whether or not the Servo is properly connected to the Maestro, for example
+		 * if a channel number was never asserted then the Servo will do nothing and return false; 
+		 */
+		bool rotateToDegrees(float degrees);
+
+
 };
